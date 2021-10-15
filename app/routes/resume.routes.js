5c66bef62ebfbc68391fc57f75eb61549dc12eea
Rahul Narvekar <rahul200023@gmail.com>
2021-10-15 08:53:57 -0400

    delete api:

--- app/routes/resume.routes.js
+++ app/routes/resume.routes.js
@@ -1,13 +1,16 @@
 module.exports = (app) => {
     const resumes = require('../controllers/resume.controller.js');
 
-    // Create a new Note
+    // Create a new Resume
     app.post('/resumes', resumes.create);
 
-    // Retrieve all Notes
+    // Retrieve all Resumes
     app.get('/resumes', resumes.findAll);
 
-    // Retrieve a single Note with noteId
+    // Retrieve a single Resume with resumeId
     app.get('/resumes/:resumeId', resumes.findOne);
+
+    // Delete a Resume with ResumeId
+    app.delete('/notes/:ResumeId', resumes.delete);
 
 }
